f4c32aa98e278659953b3819e496de678893c146
GHadder <ghadeerhabeeb2@gmail.com>
2022-12-11 16:57:57 +0300

    commit

--- Basket.dart
+++ Basket.dart
@@ -10,14 +10,16 @@ class MyBasket extends StatefulWidget {
 class _MyBasketState extends State<MyBasket> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child:
-        Container(
-          height:MediaQuery.of(context).size.height*0.5,
-          width: MediaQuery.of(context).size.width,
-          color: Colors.purpleAccent,
-        ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child:
+          Container(
+            height:MediaQuery.of(context).size.height*0.3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.purpleAccent,
+          ),
 
+      ),
     );
   }
 }
